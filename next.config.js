dd48c6881cc16746f42394b0eccdb26fd2bca48c
John Townsend <john.townsend@trojans.dsu.edu>
2022-01-23 10:42:22 -0600

    changes to try to fix next export

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 module.exports = {
   target: 'serverless',
+  images: {
+    loader: 'imgix',
+    path: '/',
+  },
   webpack: function (config) {
     config.module.rules.push({
       test: /\.md$/,
