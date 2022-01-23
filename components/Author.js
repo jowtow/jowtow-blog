1a562c46876af74bc3cb3ede91b071a021faa24a
John Townsend <john.townsend@trojans.dsu.edu>
2022-01-23 10:34:44 -0600

    fix npm run build

--- components/Author.js
+++ components/Author.js
@@ -1,12 +1,13 @@
 import styles from './Author.module.css'
+import Image from 'next/image'
 
 export default function Author() {
     return (
         <>
             <div className={styles.author}>
-                <img className={styles.authorImage} src="/images/Author.jpg"/>
+                <Image className={styles.authorImage} src="/images/Author.jpg" alt="Picture of the author."/>
                 <div className={styles.authorDescription}>
-                    <div className={styles.authorIntro}>Howdy, I'm John Townsend! Thanks for checking out the blog!</div>
+                    <div className={styles.authorIntro}>Howdy, I&apos;m John Townsend! Thanks for checking out the blog!</div>
                     <div className={styles.authorIntroCont}>I love learning new things and sharing knowledge with others. My profession is Software Engineering, but I love to learn about other trades and crafts as well.</div>
                 </div>
             </div>
