6e589d171a2e9b73aa16d02a26c3b5f110877bd6
John Townsend <john.townsend@trojans.dsu.edu>
2022-01-23 11:30:10 -0600

    still trying to fix netlify

--- components/Author.js
+++ components/Author.js
@@ -1,11 +1,14 @@
 import styles from './Author.module.css'
-import Image from 'next/image'
 
 export default function Author() {
     return (
         <>
             <div className={styles.author}>
-                <Image className={styles.authorImage} src="/images/Author.jpg" alt="Picture of the author."/>
+                <img 
+                    className={styles.authorImage} 
+                    src="/Author.jpg"
+                    alt="Picture of the author."
+                    />
                 <div className={styles.authorDescription}>
                     <div className={styles.authorIntro}>Howdy, I&apos;m John Townsend! Thanks for checking out the blog!</div>
                     <div className={styles.authorIntroCont}>I love learning new things and sharing knowledge with others. My profession is Software Engineering, but I love to learn about other trades and crafts as well.</div>
--- next.config.js
+++ next.config.js
@@ -1,9 +1,5 @@
 module.exports = {
   target: 'serverless',
-  images: {
-    loader: 'akamai',
-    path: '',
-  },
   webpack: function (config) {
     config.module.rules.push({
       test: /\.md$/,
