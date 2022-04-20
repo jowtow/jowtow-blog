dcbf852a6a3ff49c72f4b0e02732fd1c0ba0169c
John Townsend <jtownsend@omnitech-inc.com>
2022-04-20 08:52:23 -0500

    added buy me a coffee to site.

--- components/Layout.js
+++ components/Layout.js
@@ -13,6 +13,7 @@ export default function Layout({ children, pageTitle, ...props }) {
         <Header />
         <div className={styles.content}>{children}</div>
       </section>
+      <script data-name="BMC-Widget" data-cfasync="false" src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js" data-id="jowtowdev" data-description="Support me on Buy me a coffee!" data-message="Thanks for visiting, if you think I need more bean juice in my life, feel free to buy me a coffee!" data-color="#40DCA5" data-position="Right" data-x_margin="18" data-y_margin="18"></script>
       {/* <footer>Built by me!</footer> */}
     </>
   )
