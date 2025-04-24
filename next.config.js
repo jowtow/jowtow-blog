25b3e7169924f637fee674e9e9a17109db6c4b73
John Townsend <John.Townsend@trojans.dsu.edu>
2025-04-24 06:57:38 -0500

    add image loader

--- next.config.js
+++ next.config.js
@@ -2,8 +2,13 @@ module.exports = {
   webpack: function (config) {
     config.module.rules.push({
       test: /\.md$/,
-      use: 'raw-loader',
-    })
-    return config
+      use: "raw-loader",
+    });
+    return config;
   },
-}
+
+  images: {
+    loader: "akamai",
+    path: "/",
+  },
+};
