e86a3620f282b5d0de573e51921c5069b69f07b5
John Townsend <jowtow.dev@gmail.com>
2025-09-13 11:26:19 -0500

    fix author image aspect ratio

--- components/Author/Author.tsx
+++ components/Author/Author.tsx
@@ -5,19 +5,20 @@ import Image from "next/image";
 export default function Author() {
   return (
     <>
-      <div className="relative m-2 p-1 rounded border-2 border-[var(--color-primary)] flex ">
-        <Image
-          className="rounded-full m-2 mr-5"
-          src="/Author.jpg"
-          alt="Picture of the author."
-          width={100}
-          height={100}
-        />
+      <div className="relative m-2 p-1 rounded border-2 border-[var(--color-primary)] flex flex-col md:flex-row items-center">
+        <div className="relative w-[100px] h-[100px] aspect-[5/5] m-2">
+          <Image
+            className="rounded-full"
+            src="/Author.jpg"
+            alt="Picture of the author."
+            fill
+          />
+        </div>
         <div className="flex flex-col justify-center">
-          <div className="text-lg">
+          <div className="text-lg text-center">
             Howdy, I&apos;m John Townsend! Thanks for checking out the blog!
           </div>
-          <div>
+          <div className="text-center">
             I love learning new things and sharing knowledge with others. My
             profession is Software Engineering, but I love to learn about other
             trades and crafts as well.
