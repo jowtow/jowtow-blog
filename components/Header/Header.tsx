cf6e48a1d1b2aaf829e7a07b7f5e102c978beaaf
John Townsend <jowtow.dev@gmail.com>
2025-09-14 11:17:46 -0500

    small styling thing

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Header() {
   return (
     <>
-      <header className=" mb-[10px] p-[5px] bg-[url(/header.png)] bg-right bg-no-repeat bg-cover] min-h-[200px] mb-[25px] bg-[size:2500px_200px] flex justify-start xl:justify-center">
+      <header className="p-[5px] bg-[url(/header.png)] bg-right bg-no-repeat bg-cover] min-h-[200px] bg-[size:2500px_200px] flex justify-start xl:justify-center">
         <nav className="nav xl:mx-25 mx-10 my-10 bg-[var(--color-dark)] h-fit p-4 rounded">
           <Link href="/">
             <span className="text-[var(--color-primary)]">blog</span>.
