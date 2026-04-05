8392fb8c4cd995f4b482168a5b1dc59ee0796064
John Townsend <johnjohnjdubya@gmail.com>
2026-04-05 14:14:15 +0000

    update eslint packages to remove peer deps issue

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,7 @@
 import js from '@eslint/js';
-import nextConfig from 'eslint-config-next';
 
 export default [
   js.configs.recommended,
-  ...nextConfig,
   {
     ignores: [
       "node_modules/**",
